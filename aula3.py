4ff46d82559435ddeb70b456fd145a54637ea46a
Lucas Viega <lucas_viega.ti@outlook.com>
2024-08-19 20:31:05 -0300

    Meu primeiro commit

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de timpagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspoas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Texto utilizando aspas simples')
+
+# Aspas duplas
+print("Texto utilizando aspas duplas")
+
+# Escape
+print("Texto utilizando \"ESCAPE\"")
+
+# r
+print(r"Texto utilizando \\r")
