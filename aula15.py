bcf180a151bf224e074948ecfcf31d28e38030a0
Lucas Viega <lucas_viega.ti@outlook.com>
2024-08-26 14:45:39 -0300

    Aulas realizadas em 23-08

---
+++ aula15.py
@@ -0,0 +1,11 @@
+# if / elif     / else
+# se / se não se / se não
+
+entrada = input(f'Você quer entrar ou sair? \n')
+
+if entrada == 'entrar':
+    print('Você entoru no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou nem entrar nem sair')
